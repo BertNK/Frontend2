cdce0fa9f2f83687f1b967073f4ffb20427c1700
Bert <98714764+BertNK@users.noreply.github.com>
2025-05-14 15:50:12 +0200

    First commit with changes (basics and structuring)

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+function Header() {
+  return (
+    <div className="Header">
+        <img className='logo' src="/EatCheap.png"/>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from '../components/header.jsx'
+
+function Home() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  )
+}
+
+export default Home;
