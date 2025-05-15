63498c62ea0ac0e230abf8cc4a2d4346f9e58928
Bert <98714764+BertNK@users.noreply.github.com>
2025-05-15 14:39:06 +0200

    Working firebase connection

--- firebase.jsx
+++ firebase.jsx
@@ -1,11 +1,7 @@
 // Import the functions you need from the SDKs you need
+import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { logEvent } from "firebase/analytics";
 
-if (analytics) {
-  logEvent(analytics, 'notification_received');
-}
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,4 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import Header from '../components/header.jsx'
+import { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../../firebase.jsx';
+import Header from '../components/header.jsx';
 
 function Home() {
+  const [tableList, setTableList] = useState([]);
+
+  useEffect(() => {
+    const getTable = async () => {
+      const data = await getDocs(collection(db, 'recipes'));
+      setTableList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getTable();
+    // console.log(JSON.stringify(tableList))
+  },);
+
   return (
     <div className="App">
-      <Header></Header>
+      <Header />
+      <h1>Recipes</h1>
     </div>
-  )
+  );
 }
 
 export default Home;
