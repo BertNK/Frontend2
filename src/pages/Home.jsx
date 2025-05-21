4ffe559e6d1ce197340ccfc72174be2da4291f53
Bert <98714764+BertNK@users.noreply.github.com>
2025-05-21 16:38:26 +0200

    Upd 1 (First component of the home screen. will make it a seperate component soon just forgot to make it in the first place)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,24 +2,73 @@ import { useEffect, useState } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase.jsx';
 import Header from '../components/header.jsx';
+import './Home.css';
 
 function Home() {
   const [tableList, setTableList] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const getTable = async () => {
-      const data = await getDocs(collection(db, 'recipes'));
-      setTableList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+const highlightItems = [
+  {
+    img: 'https://images.unsplash.com/photo-1600891964599-f61ba0e24092?auto=format&fit=crop&w=1600&q=80',
+    link: 'https://unsplash.com/photos/VFGEZ4z8zOw', // Pizza
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1504674900247-0877df9cc836?auto=format&fit=crop&w=1600&q=80',
+    link: 'https://unsplash.com/photos/IGfIGP5ONV0', // Pasta
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1523986371872-9d3ba2e2f642?auto=format&fit=crop&w=1600&q=80',
+    link: 'https://unsplash.com/photos/pElSkGRA2NU', // Sushi
+  },
+];
 
-    getTable();
-    // console.log(JSON.stringify(tableList))
-  },);
 
-  return (
-    <div className="App">
-      <Header />
-      <h1>Recipes</h1>
+useEffect(() => {
+  const getTable = async () => {
+    const data = await getDocs(collection(db, 'recipes'));
+    setTableList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  };
+
+  getTable();
+}, []);
+
+const handlePrev = () => {
+  setCurrentIndex((prev) => (prev === 0 ? highlightItems.length - 1 : prev - 1));
+};
+
+const handleNext = () => {
+  setCurrentIndex((prev) => (prev === highlightItems.length - 1 ? 0 : prev + 1));
+};
+
+return (
+  <div className="App">
+    <Header />
+
+      <div className="highlightContainer">
+        <div className="highlightArrow" onClick={handlePrev}>
+          <img src="/arrow-up.png" className="highlightArrowImg1" alt="Prev" />
+        </div>
+
+        <div className="highlightBlock">
+          <a
+            href={highlightItems[currentIndex].link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="highlightImageLink"
+          >
+            <img
+              src={highlightItems[currentIndex].img}
+              className="highlightImage"
+              alt={`Highlight ${currentIndex + 1}`}
+            />
+          </a>
+        </div>
+
+        <div className="highlightArrow" onClick={handleNext}>
+          <img src="/arrow-up.png" className="highlightArrowImg2" alt="Next" />
+        </div>
+      </div>
     </div>
   );
 }
