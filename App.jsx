c8fabaf53c425b1dc510b3b68462130d51c3a86a
P9ndabear <1208846@student.roc-nijmegen.nl>
2025-06-04 16:01:29 +0200

    Added account page

--- App.jsx
+++ App.jsx
@@ -2,12 +2,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './src/pages/Home.jsx';
 import CreatePost from '/src/pages/CreatePost.jsx';
+import Account from "./src/pages/account.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/createPost" element={<CreatePost />} />
+      <Route path="/account" element={<Account />} />
     </Routes>
   );
 }
---
+++ src/pages/account.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/header.jsx';
+
+function account() {
+
+    return (
+        <div>
+            <Header/>
+
+        </div>
+    );
+}
+
+export default account;
